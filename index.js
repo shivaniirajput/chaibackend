527cef70feda5e8f6a83d6af7517bc984179c427
Shivani Rajput <shivanirajpoot998@gmail.com>
2024-07-11 17:34:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Chai aur code");
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('Shivani Rajput')
+})
+
+app.get('/login',(req,res) => {
+  res.send('<h1>Please login</h1>')
+})
+
+app.get('/YouTube',(req,res) => {
+  res.send('<h2>hannnnnnnnnnnnnnnn jiiiiiiiiii </h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
